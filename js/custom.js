ec73a0acc4327c7c6ec00b31f07e71f04e0ec1ca
Victor Torres Ortiz <victor.torres.ortiz.18@gmail.com>
2023-02-22 15:28:06 +0100

    Clases del video y smooth scroll realizado

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+'use strict'
+//Voy a customizar los links dandole un efecto smooth al pulsar llamando a la funcion smoothscroll
+
+(function ($) {
+
+      $('.smoothscroll').click(function(){
+        let el = $(this).attr('href');
+        let elWrapped = $(el);
+        let header_height = $('.navbar').height();
+    
+        scrollToDiv(elWrapped,header_height);
+        return false;
+    
+        function scrollToDiv(element,navheight){
+          let offset = element.offset();
+          let offsetTop = offset.top;
+          let totalScroll = offsetTop-navheight;
+    
+          $('body,html').animate({
+          scrollTop: totalScroll
+          }, 300);//Tiempo TESTEAR
+        }
+      });
+    
+    })(window.jQuery);
