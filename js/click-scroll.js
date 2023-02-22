a68b537a335e2c2f193585c3f64f8c7c3d1223b5
Victor Torres Ortiz <victor.torres.ortiz.18@gmail.com>
2023-02-22 15:13:31 +0100

    Implementación librerias jquery

---
+++ js/click-scroll.js
@@ -0,0 +1,36 @@
+'use strict'
+//Basicamente la utilidad de esta libreria es hacer una animación al pulsar click llamando a .clickscroll 
+let sectionArray = [1, 2, 3, 4, 5, 6];//Array del menu
+
+$.each(sectionArray, function(index, value){
+          
+     $(document).scroll(function(){
+         let offsetSection = $('#' + 'section_' + value).offset().top - 83;
+         let docScroll = $(document).scrollTop();
+         let docScroll1 = docScroll + 1;
+         
+        
+         if ( docScroll1 >= offsetSection ){
+             $('.navbar-nav .nav-item .nav-link').removeClass('active');
+             $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
+             $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
+             $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
+         }
+         
+     });
+    
+    $('.click-scroll').eq(index).click(function(e){
+        let offsetClick = $('#' + 'section_' + value).offset().top - 83;
+        e.preventDefault();
+        $('html, body').animate({
+            'scrollTop':offsetClick
+        }, 300)
+    });
+    
+});
+
+$(document).ready(function(){
+    $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');    
+    $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active');
+    $('.navbar-nav .nav-item .nav-link:link').eq(0).removeClass('inactive');
+});
